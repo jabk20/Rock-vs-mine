a2190c9485fad367b663e2f010f4bef261f41866
Asuwin.j <69710144+jabk20@users.noreply.github.com>
2023-08-06 13:01:31 +0530

    Add files via upload

---
+++ Rock vs Mine Prediction.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[16]:
+
+
+get_ipython().system('pip install numpy')
+get_ipython().system('pip install pandas')
+get_ipython().system('pip install scikit-learn')
+
+
+# In[18]:
+
+
+import numpy as np
+import pandas as pd
+import sklearn
+
+
+# In[3]:
+
+
+sonar_data = pd.read_csv("C:\\Users\\asuwi\\Downloads\\Copy of sonar data.csv", header = None)
+
+
+# In[4]:
+
+
+sonar_data.head()
+
+
+# In[5]:
+
+
+sonar_data.shape
+
+
+# In[6]:
+
+
+sonar_data.describe()
+
+
+# In[7]:
+
+
+sonar_data[60].value_counts()
+
+
+# In[8]:
+
+
+sonar_data.groupby(60).mean()
+
+
+# In[9]:
+
+
+X = sonar_data.drop(columns=60, axis=1)
+Y = sonar_data[60]
+
+
+# In[12]:
+
+
+print(X)
+print(Y)
+
+
+# In[50]:
+
+
+#training and testing
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+X_tr, X_te, Y_tr, Y_te = train_test_split(X, Y,test_size = 0.2, stratify=Y, random_state=1)
+
+
+# In[51]:
+
+
+print(X.shape, X_tr.shape, X_te.shape)
+
+
+# In[52]:
+
+
+print(X_tr)
+
+
+# In[53]:
+
+
+print(Y_tr)
+
+
+# In[54]:
+
+
+#Model Training
+model = LogisticRegression()
+
+
+# In[55]:
+
+
+model.fit(X_tr, Y_tr)
+
+
+# In[56]:
+
+
+#Model Evaluation
+X_tr_prediction = model.predict(X_tr)
+tr_data_accuracy = accuracy_score(X_tr_prediction, Y_tr)
+
+
+# In[57]:
+
+
+print("Accuracy on training data:", tr_data_accuracy)
+
+
+# In[58]:
+
+
+X_te_prediction = model.predict(X_te)
+te_data_accuracy = accuracy_score(X_te_prediction, Y_te)
+
+
+# In[59]:
+
+
+print("Accuracy on testing data:", te_data_accuracy)
+
+
+# In[60]:
+
+
+input_data = (0.0307,0.0523,0.0653,0.0521,0.0611,0.0577,0.0665,0.0664,0.1460,0.2792,0.3877,0.4992,0.4981,0.4972,0.5607,0.7339,0.8230,0.9173,0.9975,0.9911,0.8240,0.6498,0.5980,0.4862,0.3150,0.1543,0.0989,0.0284,0.1008,0.2636,0.2694,0.2930,0.2925,0.3998,0.3660,0.3172,0.4609,0.4374,0.1820,0.3376,0.6202,0.4448,0.1863,0.1420,0.0589,0.0576,0.0672,0.0269,0.0245,0.0190,0.0063,0.0321,0.0189,0.0137,0.0277,0.0152,0.0052,0.0121,0.0124,0.0055)
+
+# changing the input_data to a numpy array
+input_data_as_numpy_array = np.asarray(input_data)
+
+# reshape the np array as we are predicting for one instance
+input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+prediction = model.predict(input_data_reshaped)
+print(prediction)
+
+if (prediction[0]=='R'):
+  print('The object is a Rock')
+else:
+  print('The object is a mine')
+
+
+# In[ ]:
+
+
+
+
